8988f991cf6261273e3b9868283a30c5ff9eea76
dev | HOME <gokhan.plt@gmail.com>
2022-11-30 16:24:03 +0300

    ADDED - solc 0.6 ABIEncoderV2 pragma for new features

--- src/10_Re-entrance/test/ReentranceTest.t.sol
+++ src/10_Re-entrance/test/ReentranceTest.t.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
+pragma experimental ABIEncoderV2;
+
 import "forge-std/src/Test.sol";
 
 // Custom test utils, createUser, mineBlocks etc.
--- test/utils/Utils.sol
+++ test/utils/Utils.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.12 <=0.9.0;
 
+pragma experimental ABIEncoderV2;
+
 import "forge-std/src/Test.sol";
 
 /* solhint-disable private-vars-leading-underscore */
